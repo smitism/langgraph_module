f3be2fb6a40073bbde17a1b5934c0d6fd1c11070
smit <smithajgude2006@gmail.com>
2026-07-20 19:19:16 +0530

    first react agent

---
+++ agent-3.py
@@ -0,0 +1,76 @@
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage  # The foundational class for all message types in LangGraph
+from langchain_core.messages import ToolMessage  # Passes data back to LLM after it calls a tool such as the content and the result
+from langchain_core.messages import SystemMessage  # Message for providing instructions to the LLM
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
+from langchain_google_genai import ChatGoogleGenerativeAI
+#this will be a ReAcT agent that means it'll have tools
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages : Annotated[Sequence[BaseMessage],add_messages]
+
+@tool
+def add(a:int,b:int):
+    """this is an addition where it adds two number"""
+    return a+b
+
+tools = [add]
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash").bind_tools(tools)
+
+def model_call(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(
+        content="You are my AI assistant, please answer my query to the best of your ability."
+    )
+
+    response = model.invoke([system_prompt]+state["messages"])
+
+    return {"messages": [response]}
+
+def should_continue(state:AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+    
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END,
+    },
+)
+
+graph.add_edge("tools","our_agent")
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+
+inputs = {"messages": [("user", "Add 3 + 4.")]}
+print_stream(app.stream(inputs, stream_mode="values"))
+
